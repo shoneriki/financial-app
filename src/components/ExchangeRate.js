e6733370c7822ca931165e415d137165912cb1bc
Sho Neriki <ikirenohs@gmail.com>
2023-04-17 12:02:02 +0900

    sorting out layout issues

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -13,27 +13,28 @@ const ExchangeRate = ({
   const roundedResult = Math.round(result * 100) / 100;
   const roundedAmount = Math.round(amount * 100) / 100;
   return (
-      <div className="exchange-rate-display">
+    <div className="exchange-rate-display">
       {(result !== 0) | null && exchangeRate !== 0 ? (
-        <div className="exchange-rate">
-            <h3>Exchange Rate: {roundedExchange}</h3>
-            <h3>
-              {Currencies.map((currency) => {
-                if (currency.value === primaryCurrency) {
-                  return currency.sign;
-                }
-              })}
-              {roundedAmount} ={" "}
-              {Currencies.map((currency) => {
-                if (currency.value === secondaryCurrency) {
-                  return currency.sign;
-                }
-              })}
-              {roundedResult}
-            </h3>
-        </div>
-      ) : null}
+          <div className="exchange-rate">
+              <h3>Exchange Rate: {roundedExchange}</h3>
+              <h3>
+                {Currencies.map((currency) => {
+                  if (currency.value === primaryCurrency) {
+                    return currency.sign;
+                  }
+                })}
+                {roundedAmount} ={" "}
+                {Currencies.map((currency) => {
+                  if (currency.value === secondaryCurrency) {
+                    return currency.sign;
+                  }
+                })}
+                {roundedResult}
+              </h3>
+          </div>
+        ) : null}
       </div>
+
   );
 };
 
